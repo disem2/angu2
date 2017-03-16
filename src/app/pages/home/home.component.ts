0e13bf6030eb02de70b3fd9e6734fe85b9ad4b1c
Dmytro_Semenov <dmytro_semenov@epam.com>
2017-03-16 18:46:30 +0200

    added curse item service methods

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -18,37 +18,10 @@ export class HomeComponent implements OnInit {
   }
 
   public ngOnInit() {
-    const coursesMockData = [
-      {
-        title: 'Course 1',
-        id: '1',
-        duration: 50,
-        addingDate: new Date(),
-        description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been 
-          the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley`
-      },
-      {
-        title: 'Course 2',
-        id: '2',
-        duration: 500,
-        addingDate: new Date(),
-        description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been 
-          the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley`
-      },
-      {
-        title: 'Course 3',
-        id: '3',
-        duration: 10,
-        addingDate: new Date(),
-        description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been 
-          the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley`
-      },
-    ];
-
-    this.courses = this.courseService.prepareCourses(coursesMockData);
+    this.courses = this.courseService.getCourses();
   }
 
   public removeCourse(id) {
-    console.log(id);
+    this.courseService.removeCourse(id);
   }
 }
--- src/app/shared/services/course.service.ts
+++ src/app/shared/services/course.service.ts
@@ -3,15 +3,36 @@ import { Injectable } from '@angular/core';
 import { CourseItemClass } from '../components/course-item/course-item.class';
 import { CourseInterface } from '../../shared/interfaces/course.interface';
 
+const coursesMockData = [
+  {
+    title: 'Course 1',
+    id: '1',
+    duration: 50,
+    addingDate: new Date(),
+    description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been 
+          the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley`
+  },
+  {
+    title: 'Course 2',
+    id: '2',
+    duration: 500,
+    addingDate: new Date(),
+    description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been 
+          the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley`
+  },
+  {
+    title: 'Course 3',
+    id: '3',
+    duration: 10,
+    addingDate: new Date(),
+    description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been 
+          the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley`
+  },
+];
+
 @Injectable()
 export class CourseService {
-  private courses;
-
-  constructor() {
-    this.courses = [];
-  }
-
-  public prepareCourses(data: CourseInterface[]) {
+  private static getPreparedCourses(data: CourseInterface[]) {
     const courses = [];
 
     for (let course of data) {
@@ -28,12 +49,52 @@ export class CourseService {
       courses.push(courseItem);
     }
 
-    this.courses = courses;
-
     return courses;
+  }
+
+  private courses;
+
+  constructor() {
+    this.courses = CourseService.getPreparedCourses(coursesMockData);
   }
 
   public getCourses() {
     return this.courses;
+  }
+
+  public getCourseById(id: string): CourseInterface {
+    for(const course of this.courses) {
+      if (course.id === id) {
+        return course;
+      }
+    }
+  }
+
+  public createCourse(data: CourseInterface) {
+    const course = {
+      title: data.title,
+      id: data.id,
+      duration: data.duration,
+      addingDate: data.addingDate,
+      description: data.description
+    };
+
+    this.courses.push(course);
+  }
+
+  public updateCourse(id: string, newData: CourseInterface) {
+    for(let i = 0; i < this.courses.length; i++) {
+      if (this.courses[i].id === id) {
+        this.courses[i] = newData;
+      }
+    }
+  }
+
+  public removeCourse(id: string) {
+    for(let i = 0; i < this.courses.length; i++) {
+      if (this.courses[i].id === id) {
+        this.courses.splice(i, 1);
+      }
+    }
   }
 }
